2b32d0d6af8deadba030176b81d950f752020b22
Arseniy Ivanov <freeatnet@freeatnet.com>
2016-12-05 22:19:50 -0500

    fixup! Code update for propagator testing

--- orderings.py
+++ orderings.py
@@ -10,7 +10,7 @@ bt_search.
 
 var_ordering == a function with the following template
     ord_type(csp)
-        ==> returns Variable 
+        ==> returns Variable
 
     csp is a CSP object---the heuristic can use this to get access to the
     variables and constraints of the problem. The assigned variables can be
@@ -22,13 +22,16 @@ var_ordering == a function with the following template
 val_ordering == a function with the following template
     val_ordering(csp,var)
         ==> returns [Value, Value, Value...]
-    
-    csp is a CSP object, var is a Variable object; the heuristic can use csp to access the constraints of the problem, and use var to access var's potential values. 
+
+    csp is a CSP object, var is a Variable object; the heuristic can use csp to access the constraints of the problem, and use var to access var's potential values.
 
     val_ordering returns a list of all var's potential values, ordered from best value choice to worst value choice according to the heuristic.
 
 '''
 
+
+def ord_sequential(csp):
+    return csp.get_all_unasgn_vars()[0]
 
 def ord_random(csp):
     '''
@@ -51,9 +54,9 @@ def val_arbitrary(csp,var):
 def ord_mrv(csp):
     '''
     ord_mrv(csp):
-    A var_ordering function that takes CSP object csp and returns Variable object var, 
-    according to the Minimum Remaining Values (MRV) heuristic as covered in lecture.  
-    MRV returns the variable with the most constrained current domain 
+    A var_ordering function that takes CSP object csp and returns Variable object var,
+    according to the Minimum Remaining Values (MRV) heuristic as covered in lecture.
+    MRV returns the variable with the most constrained current domain
     (i.e., the variable with the fewest legal values).
     '''
 #IMPLEMENT
@@ -75,11 +78,11 @@ def ord_dh(csp):
     ord_dh(csp):
     A var_ordering function that takes CSP object csp and returns Variable object var,
     according to the Degree Heuristic (DH), as covered in lecture.
-    Given the constraint graph for the CSP, where each variable is a node, 
+    Given the constraint graph for the CSP, where each variable is a node,
     and there exists an edge from two variable nodes v1, v2 iff there exists
     at least one constraint that includes both v1 and v2,
     DH returns the variable whose node has highest degree.
-    '''    
+    '''
 #IMPLEMENT
     usasgn_vars = csp.get_all_unasgn_vars()
     highest_degree = -1
@@ -102,13 +105,13 @@ def val_lcv(csp,var):
     val_lcv(csp,var):
     A val_ordering function that takes CSP object csp and Variable object var,
     and returns a list of Values [val1,val2,val3,...]
-    from var's current domain, ordered from best to worst, evaluated according to the 
+    from var's current domain, ordered from best to worst, evaluated according to the
     Least Constraining Value (LCV) heuristic.
-    (In other words, the list will go from least constraining value in the 0th index, 
-    to most constraining value in the $j-1$th index, if the variable has $j$ current domain values.) 
-    The best value, according to LCV, is the one that rules out the fewest domain values in other 
+    (In other words, the list will go from least constraining value in the 0th index,
+    to most constraining value in the $j-1$th index, if the variable has $j$ current domain values.)
+    The best value, according to LCV, is the one that rules out the fewest domain values in other
     variables that share at least one constraint with var.
-    '''    
+    '''
 #IMPLEMENT
     # Dict of all possible value v of var, v:int, int is the number of domain values that rules out if pick v
     d = dict((v, 0) for v in var.cur_domain())
@@ -123,7 +126,7 @@ def val_lcv(csp,var):
                             for t in con.sup_tuples[(other_var, other_var_value)]:
                                 if con.tuple_is_valid(t) and (t not in con.sup_tuples[(var, value)]):
                                     d[value] += 1
- 
+
 
             else:
                 d[value] = math.inf
@@ -137,9 +140,9 @@ def ord_custom(csp):
     '''
     ord_custom(csp):
     A var_ordering function that takes CSP object csp and returns Variable object var,
-    according to a Heuristic of your design.  This can be a combination of the ordering heuristics 
+    according to a Heuristic of your design.  This can be a combination of the ordering heuristics
     that you have defined above.
-    '''    
+    '''
 #IMPLEMENT
 
     # Use MRV. If there is a tie, use DH to determine
@@ -167,5 +170,3 @@ def ord_custom(csp):
                 highest_degree = v_degree
 
     return result
-
-
